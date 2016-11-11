dad8fc3956714c4008e343fe0df5bcc3e3cb1b9a
Henry Pitcairn <735tesla@gmail.com>
2016-11-11 15:24:40 -0500

    Initial commit

---
+++ src/main/java/org/usfirst/frc/team4373/robot/Robot.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team4373.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * Created by tesla on 10/22/16.
+ */
+public class Robot extends IterativeRobot {
+    
+}
