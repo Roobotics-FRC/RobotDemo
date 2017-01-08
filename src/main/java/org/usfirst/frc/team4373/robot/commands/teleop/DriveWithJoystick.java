0de597cafcb79a84ab374218fe2247b7cf547c81
Henry Pitcairn <735tesla@gmail.com>
2017-01-08 12:42:43 -0500

    Filled in DriveWithJoystick methods

--- src/main/java/org/usfirst/frc/team4373/robot/commands/teleop/DriveWithJoystick.java
+++ src/main/java/org/usfirst/frc/team4373/robot/commands/teleop/DriveWithJoystick.java
@@ -1,20 +1,30 @@
 package org.usfirst.frc.team4373.robot.commands.teleop;
 
 import org.usfirst.frc.team4373.robot.commands.CommandBase;
+import org.usfirst.frc.team4373.robot.input.hid.RooJoystick;
+import org.usfirst.frc.team4373.robot.subsystems.DriveTrain;
 
 /**
  * @author (tesla)
  * Created on 1/8/17
  */
 public class DriveWithJoystick extends CommandBase {
+    private DriveTrain driveTrain;
+    private RooJoystick joystick;
+
+
     @Override
     protected void initialize() {
-        
+        driveTrain = DriveTrain.getDriveTrain();
+        joystick = oi.getDriveJoystick();
     }
 
     @Override
     protected void execute() {
-
+        double right = this.joystick.getAxis(1) - this.joystick.getAxis(3);
+        double left = this.joystick.getAxis(1) + this.joystick.getAxis(3);
+        driveTrain.setLeft(left);
+        driveTrain.setRight(right);
     }
 
     @Override
@@ -29,6 +39,6 @@ public class DriveWithJoystick extends CommandBase {
 
     @Override
     protected void interrupted() {
-
+        this.driveTrain.setBoth(0);
     }
 }
