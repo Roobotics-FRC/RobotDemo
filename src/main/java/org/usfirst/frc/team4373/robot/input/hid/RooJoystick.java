48ce96a3bb300fdb95089729f6be6d1b3f4ce253
Henry Pitcairn <735tesla@gmail.com>
2017-01-08 11:54:40 -0500

    Added RooJoystick class from 2016

---
+++ src/main/java/org/usfirst/frc/team4373/robot/input/hid/RooJoystick.java
@@ -0,0 +1,107 @@
+package org.usfirst.frc.team4373.robot.input.hid;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import org.usfirst.frc.team4373.robot.input.filter.Filter;
+
+/**
+ * Created by tesla on 11/1/15.
+ */
+
+/**
+ * This class extends the WPILib Joystick class
+ * to add deadzone and filter functionality.
+ * @author (Henry Pitcairn)
+ */
+public class RooJoystick extends Joystick {
+    private static final double DEADZONE = 0.08D;
+    private Filter filter = null;
+
+    public RooJoystick(int port, Filter... filter) {
+        super(port);
+        this.filter = filter.length > 0 ? filter[0] : null;
+    }
+
+    private double filter(double val, Filter filter) {
+        Object ret;
+        if (filter == null) {
+            if (this.filter == null) {
+                ret = val;
+            } else {
+                ret = this.filter.applyFilter(val);
+            }
+        } else {
+            ret = filter.applyFilter(val);
+        }
+        return applyDeadzone((Double) ret);
+    }
+
+    private double applyDeadzone(double input) {
+        return Math.abs(input) <= DEADZONE ? 0 : input;
+    }
+
+    private double rooGetX(Filter filter) {
+        return this.filter(this.getX(), filter);
+    }
+
+    private double rooGetY(Filter filter) {
+        return this.filter(this.getY(), filter);
+    }
+
+    private double rooGetZ(Filter filter) {
+        return this.filter(this.getZ(), filter);
+    }
+
+    private double rooGetTwist(Filter filter) {
+        return this.filter(this.getTwist(), filter);
+    }
+
+    private double rooGetThrottle(Filter filter) {
+        return this.filter(this.getThrottle(), filter);
+    }
+
+    @Override
+    public double getAxis(Joystick.AxisType axis) {
+        return this.getAxis(axis.value);
+    }
+
+    public double getAxis(Joystick.AxisType axis, Filter... filter) {
+        return this.filter(this.getAxis(axis.value), filter.length > 0 ? filter[0] : null);
+    }
+
+    public double getAxis(int axis, Filter... filter) {
+        return this.filter(this.getAxis(axis), filter.length > 0 ? filter[0] : null);
+    }
+
+    /**
+     * Returns the filtered value of a joystick access.
+     * @param axis the axis to read from
+     * @return the filtered value of the axis
+     */
+    public double getAxis(int axis) {
+        switch (axis) {
+            case 0:
+                return this.rooGetX(null);
+            case 1:
+                return this.rooGetY(null);
+            case 2:
+                return this.rooGetZ(null);
+            case 3:
+                return this.rooGetTwist(null);
+            case 4:
+                return this.rooGetThrottle(null);
+            default:
+                return 0.0;
+        }
+    }
+
+    /**
+     * Gets the angle the joystick is facing relative to neutral.
+     * @return the joystick angle
+     */
+    public double getAngle() {
+        double x = this.getAxis(AxisType.kX);
+        double y = this.getAxis(AxisType.kY);
+        return Math.atan(y / x);
+    }
+}
