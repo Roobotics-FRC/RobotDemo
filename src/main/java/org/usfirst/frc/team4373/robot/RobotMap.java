38698479c97703a45ccc2ebb7ce84e787279d546
Henry Pitcairn <735tesla@gmail.com>
2017-01-08 14:35:42 -0500

    Added comment to RobotMap

--- src/main/java/org/usfirst/frc/team4373/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team4373/robot/RobotMap.java
@@ -4,6 +4,7 @@ package org.usfirst.frc.team4373.robot;
  * Created by tesla on 10/22/16.
  */
 public class RobotMap {
+    // Sensor ports
     public static final int JOYSTICK_PORT = 0;
 
     // Motor ports
