f3d72e5ee9c2f3f42d08661728d49020ae0b3517
Henry Pitcairn <735tesla@gmail.com>
2017-01-08 12:04:53 -0500

    Added DriveWithJoystick command stub

---
+++ src/main/java/org/usfirst/frc/team4373/robot/commands/teleop/DriveWithJoystick.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team4373.robot.commands.teleop;
+
+import org.usfirst.frc.team4373.robot.commands.CommandBase;
+
+/**
+ * @author (tesla)
+ * Created on 1/8/17
+ */
+public class DriveWithJoystick extends CommandBase {
+    @Override
+    protected void initialize() {
+        
+    }
+
+    @Override
+    protected void execute() {
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+
+    }
+
+    @Override
+    protected void interrupted() {
+
+    }
+}
