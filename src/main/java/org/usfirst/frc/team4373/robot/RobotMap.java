317f13339a6ef2998b17f1716801ba406f48ac27
aaplmath <aaplmath@users.noreply.github.com>
2017-01-08 12:32:13 -0500

    Make DriveTrain compile

--- src/main/java/org/usfirst/frc/team4373/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team4373/robot/RobotMap.java
@@ -5,4 +5,10 @@ package org.usfirst.frc.team4373.robot;
  */
 public class RobotMap {
     public static final int JOYSTICK_PORT = 0;
+
+    // Motor ports
+    public static final int LEFT_DRIVE_MOTOR_1 = 2;
+    public static final int LEFT_DRIVE_MOTOR_2 = 4;
+    public static final int RIGHT_DRIVE_MOTOR_1 = 3;
+    public static final int RIGHT_DRIVE_MOTOR_2 = 1;
 }
--- src/main/java/org/usfirst/frc/team4373/robot/subsystems/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team4373/robot/subsystems/DriveTrain.java
@@ -1,8 +1,40 @@
 package org.usfirst.frc.team4373.robot.subsystems;
 
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team4373.robot.RobotMap;
+import org.usfirst.frc.team4373.robot.commands.teleop.DriveWithJoystick;
+
 /**
  * @author (tesla)
  * Created on 1/8/17
  */
-public class DriveTrain {
+public class DriveTrain extends Subsystem {
+    private CANTalon left1, left2, right1, right2;
+
+    public DriveTrain() {
+        super("DriveTrain");
+        this.left1 = new CANTalon(RobotMap.LEFT_DRIVE_MOTOR_1);
+        this.left2 = new CANTalon(RobotMap.LEFT_DRIVE_MOTOR_2);
+        this.right1 = new CANTalon(RobotMap.RIGHT_DRIVE_MOTOR_1);
+        this.right2 = new CANTalon(RobotMap.RIGHT_DRIVE_MOTOR_2);
+    }
+
+    public void setLeft(double power) {
+
+    }
+
+    public void setRight(double power) {
+
+    }
+
+    public void setBoth(double power) {
+
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        setDefaultCommand(new DriveWithJoystick());
+    }
+
 }
