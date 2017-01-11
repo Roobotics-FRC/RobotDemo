68c758328b98ca74c22a2472324e507d283a7228
Henry Pitcairn <735tesla@gmail.com>
2017-01-10 19:03:03 -0500

    Added ADXRS450 gyro sensor code to OI

--- src/main/java/org/usfirst/frc/team4373/robot/OI.java
+++ src/main/java/org/usfirst/frc/team4373/robot/OI.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4373.robot;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import org.usfirst.frc.team4373.robot.input.filter.CubeFilter;
 import org.usfirst.frc.team4373.robot.input.hid.RooJoystick;
 
@@ -16,6 +17,7 @@ public class OI {
     }
 
     private RooJoystick driveJoystick;
+    private ADXRS450_Gyro gyro;
 
     private OI() {
         this.driveJoystick = new RooJoystick(RobotMap.JOYSTICK_PORT, new CubeFilter());
@@ -23,5 +25,9 @@ public class OI {
 
     public RooJoystick getDriveJoystick() {
         return this.driveJoystick;
+    }
+
+    public ADXRS450_Gyro getGyro() {
+        return gyro;
     }
 }
